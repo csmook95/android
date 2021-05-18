85bae42ac6043558432d1e114707057bcd61dd92
Seungmook <csmook95@gmail.com>
2021-05-18 12:35:09 +0900

    2021.05.17.mon fragment

---
+++ doit/my/part2/chapter05/SampleFragment/app/src/main/java/study/android/samplefragment/MainFragment.java
@@ -0,0 +1,34 @@
+package study.android.samplefragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main,container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onFragmentChanged(0);
+            }
+        });
+
+        return rootView;
+    }
+}
